7c8e841e3c34748108aaa68d5c9d8978dac2f007
Hoàng Minh Chiến <hmc2362008@gmail.com>
2024-10-14 01:43:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 window.onload = function() {
     document.getElementById('loading').style.display = 'none'; // Ẩn loading
     document.querySelector('.container').style.display = 'block'; // Hiện nội dung chính
-    document.getElementById('background-music').play(); // Phát nhạc nền tự động
+    // Âm thanh đã được phát tự động nhờ thuộc tính autoplay
 };
